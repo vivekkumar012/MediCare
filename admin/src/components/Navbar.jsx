385912b0cba5d28990859659092b6e93979c1314
Vivek Kumar <vh118854@gmail.com>
2026-05-28 22:43:51 +0530

    updated

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useRef, useState } from "react";
+import { navbarStyles as ns } from "../assets/dummyStyles.js";
+import logoImg from "../assets/logo.png";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+  const navInnerRef = useRef(null);
+  
   return (
-    <div>
-      Navbar
-    </div>
-  )
-}
+    <header className={ns.header}>
+      <nav className={ns.navContainer}>
+        <div className={ns.flexContainer}>
+          <div className={ns.logoContainer}>
+            <img src={logoImg} alt="logo" className={ns.logoImage} />
 
-export default Navbar
+            <Link to={"/"}>
+              <div className={ns.logoLink}>MediCare</div>
+              <div className={ns.logoSubtext}>Healthcare Solutios</div>
+            </Link>
+          </div>
+
+          <div className={ns.centerNavContainer}>
+            <div className={ns.glowEffect}>
+              <div className={ns.centerNavInner}>
+                <div></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
