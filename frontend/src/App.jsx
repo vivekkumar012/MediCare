3aba5c7f4cd6430f1a9098a6732f4d4738a15a79
Vivek Kumar <vh118854@gmail.com>
2026-06-11 19:55:46 +0530

    added new pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import Doctors from './pages/Doctors'
+import DoctorDetails from './pages/DoctorDetails'
+import Service from './pages/Service'
 
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/doctors' element={<Doctors />} />
+        <Route path='/doctors/:id' element={<DoctorDetails />} />
+        <Route path='/services' element={<Service />} />
       </Routes>
     </div>
   )
---
+++ frontend/src/components/ServicePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { servicePageStyles, serviceCardStyles } from '../assets/dummyStyles'
+
+const ServicePage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ServicePage
