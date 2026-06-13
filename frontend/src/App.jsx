ffd3d1eeb0f4e2d1f1e600bcd54926933fb0bb77
Vivek Kumar <vh118854@gmail.com>
2026-06-13 19:34:28 +0530

    doctors page completed hai

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,33 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import Doctors from './pages/Doctors'
-import DoctorDetails from './pages/DoctorDetails'
-import Service from './pages/Service'
-import ServiceDetailPage from './pages/ServiceDetailPage'
-import Contact from './pages/Contact'
-import Login from './pages/Login'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Doctors from "./pages/Doctors";
+import DoctorDetails from "./pages/DoctorDetails";
+import Service from "./pages/Service";
+import ServiceDetailPage from "./pages/ServiceDetailPage";
+import Contact from "./pages/Contact";
+import Login from "./pages/Login";
+import DHome from "./pages/DHome";
+import List from "./doctor/List";
+import EditProfile from "./doctor/EditProfile";
 
 const App = () => {
   return (
     <div>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/doctors' element={<Doctors />} />
-        <Route path='/doctors/:id' element={<DoctorDetails />} />
-        <Route path='/services' element={<Service />} />
-        <Route path='/services/:id' element={<ServiceDetailPage />} />
-        <Route path='/contact' element={<Contact />} />
-        <Route path='/doctor-admin/login' element={<Login/> } />
+        <Route path="/" element={<Home />} />
+        <Route path="/doctors" element={<Doctors />} />
+        <Route path="/doctors/:id" element={<DoctorDetails />} />
+        <Route path="/services" element={<Service />} />
+        <Route path="/services/:id" element={<ServiceDetailPage />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/doctor-admin/login" element={<Login />} />
+        <Route path="/doctor-admin/:id" element={<DHome />} />
+        <Route path="/doctor-admin/:id/appointments" element={<List />} />
+        <Route path="/doctor-admin/:id/profile/edit" element={<EditProfile />} />
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ frontend/src/pages/DHome.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import DoctorNavbar from '../doctor/DoctorNavbar'
+import DashBoardPage from '../doctor/DashBoardPage'
+
+const DHome = () => {
+  return (
+    <div>
+      <DoctorNavbar />
+      <DashBoardPage />
+    </div>
+  )
+}
+
+export default DHome
