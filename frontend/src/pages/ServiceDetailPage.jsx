80db8be361ee0a022d471aec529f710dd6d577f3
Vivek Kumar <vh118854@gmail.com>
2026-06-14 15:52:31 +0530

    urls added

--- frontend/src/pages/ServiceDetailPage.jsx
+++ frontend/src/pages/ServiceDetailPage.jsx
@@ -12,7 +12,7 @@ import { useAuth } from "@clerk/react";
 import toast, { Toaster } from "react-hot-toast";
 import { serviceDetailStyles, iconSize } from "../assets/dummyStyles";
 
-const DEFAULT_HOST = "http://localhost:4000".replace(/\/$/, "");
+const DEFAULT_HOST = "https://medicare-backend-wgf4.onrender.com".replace(/\/$/, "");
 
 export default function ServiceDetail() {
   const { id } = useParams();
